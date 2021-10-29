28822b27f0fa679156f049c210c3599db90397d6
Billal Haddad <BILLAL.HADDAD@local.ittcesena.it>
2021-10-29 09:04:16 +0200

    Brano

---
+++ Brano.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazioneGIT
+{
+    class Brano
+    {
+        private string _titolo;
+        private string _autore;
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        private float _durata;
+        public float Durata
+        {
+            get
+            {
+                return _durata;
+            }
+            set
+            {
+                if (value > 0)
+                    _durata = value;
+                else
+                    throw new Exception("Errore");
+            }
+        }
+        public Brano(float durata,string titolo,string autore)
+        {
+            Durata = durata;
+            Titolo = titolo;
+            Autore = autore;
+        }
+        public bool ShortSong(float durata)
+        {
+            if (_durata < durata)
+                return true;
+            else
+                return false;
+        }
+        public override string ToString()
+        {
+            return Durata +" "+ Titolo +" "+ Autore;
+        }
+
+    }
+}
